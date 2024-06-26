602710cf0e890b7cb4447825ea611c0b0d6589d0
Henadz <gdavydchik@mail.ru>
2024-06-26 20:15:50 +0300

    Update provider.tf

--- terraform-iac/1-provider.tf
+++ terraform-iac/1-provider.tf
@@ -1,12 +1,13 @@
 provider "google" {
-  project = "gendevby"
-  region  = "europe-west1"
-  zone    = "europe-west1-b"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
+
 terraform {
   backend "gcs" {
-    bucket = "gendevby-tf-state"
-    prefix = "terraforn/state"
+    bucket = "gendevby-427512"
+    prefix = "terraform/state"
   }
   required_providers {
     google = {
